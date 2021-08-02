204150a4646e350216d9a6b1fc367ff9debd0509
Dorin Duminica <duminicadorin@gmail.com>
2021-08-02 11:22:57 +0200

    improved two finger long press detection for fat fingers

--- app/src/main/java/com/opensourcesoftware/mobiletouchpad/TouchpadGestures.java
+++ app/src/main/java/com/opensourcesoftware/mobiletouchpad/TouchpadGestures.java
@@ -96,11 +96,17 @@ public class TouchpadGestures extends GestureDetector.SimpleOnGestureListener
 
     private class UILongPressGestureRecognizer {
 
+        private final String TAG = "UILongPressGestureRecog";
+
         private UILongPressGestureRecognizerListener mListener = null;
         private final Handler mHandler = new Handler();
         private Runnable mLongPressed = () -> mListener.onLongPressEvent();
         private long mTouchDownTS;
         private int mLongPressTimeout = 500;
+        private boolean mInitLastXY = false;
+        private boolean mHasCallbacks = false;
+        private float mLastX = 0.f;
+        private float mLastY = 0.f;
 
         public UILongPressGestureRecognizer(UILongPressGestureRecognizerListener longPressListener) {
             this.mListener = longPressListener;
@@ -108,8 +114,12 @@ public class TouchpadGestures extends GestureDetector.SimpleOnGestureListener
         }
 
         private void resetHandler(boolean addCallback) {
+            mHasCallbacks = false;
             mHandler.removeCallbacks(mLongPressed);
-            if (addCallback) mHandler.postDelayed(mLongPressed, mLongPressTimeout);
+            if (addCallback) {
+                mHandler.postDelayed(mLongPressed, mLongPressTimeout);
+                mHasCallbacks = true;
+            }
         }
 
         public boolean onTouchEvent(MotionEvent event) {
@@ -120,16 +130,25 @@ public class TouchpadGestures extends GestureDetector.SimpleOnGestureListener
                     resetHandler(true);
                     break;
                 case MotionEvent.ACTION_POINTER_DOWN:
-//                    Log.d(TAG, "UILongPressGestureRecognizer onTouchEvent: ACTION_POINTER_DOWN");
+//                    Log.d(TAG, "onTouchEvent: ACTION_POINTER_DOWN");
                     resetHandler(true);
                     mTouchDownTS = System.currentTimeMillis();
                     break;
                 case MotionEvent.ACTION_MOVE:
+                    float currX = event.getX();
+                    float currY = event.getY();
+//                    Log.d(TAG, "onTouchEvent: ACTION_MOVE " + currX + " / " + currY);
                     long tsSinceTouchDown = System.currentTimeMillis() - mTouchDownTS;
-                    if ((tsSinceTouchDown < 60))
+                    boolean ignoreMovement = ((Math.abs(currX - mLastX) < 5) && (Math.abs(currY - mLastY) < 5));
+                    if ((tsSinceTouchDown < 60) || (!mInitLastXY) || ignoreMovement) {
+                        mInitLastXY = true;
+                        mLastX = currX;
+                        mLastY = currY;
+                        if (!mHasCallbacks) resetHandler(true);
                         break;
+                    }
                 case MotionEvent.ACTION_UP:
-//                    Log.d(TAG, "UILongPressGestureRecognizer onTouchEvent: " + (action == MotionEvent.ACTION_MOVE ? "ACTION_MOVE" : "ACTION_UP"));
+//                    Log.d(TAG, "onTouchEvent: ACTION_UP");
                     resetHandler(false);
                     break;
             }
